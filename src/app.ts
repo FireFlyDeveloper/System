325bff2211a22f3a60b44bee868ba1d05b53db9d
Kim <saludeskimdev@gmail.com>
2025-05-06 12:28:41 +0800

    Refactor session management; implement sessions middleware and update routes

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,45 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
+import { CookieStore, Session, sessionMiddleware } from "hono-sessions";
+import { SessionDataTypes } from "./model/types";
 import { serveHTML } from "./utils/serverHTML";
-import { sessionMiddleware } from "./middlewares/sessionMiddleware";
+import { sessionsMiddleware } from "./middlewares/sessionMiddleware";
 
-const app = new Hono();
+const app = new Hono<{
+  Variables: {
+    session: Session<SessionDataTypes>;
+  };
+}>();
+
+const store = new CookieStore();
+
+app.use(
+  "*",
+  sessionMiddleware({
+    store,
+    encryptionKey: process.env.ENCRYPTION_KEY,
+    expireAfterSeconds: 2592000,
+    cookieOptions: {
+      sameSite: "Lax",
+      path: "/",
+      httpOnly: true,
+    },
+  }),
+);
 
 app.use("/static/*", serveStatic({ root: "./src/" }));
 
-app.get("/", sessionMiddleware, (c) => c.html(serveHTML("login.html")));
-app.get("/dashboard", sessionMiddleware, (c) => c.html(serveHTML("home.html")));
-app.get("/dashboard/alerts", sessionMiddleware, (c) =>
+app.get("/", sessionsMiddleware, (c) => c.html(serveHTML("login.html")));
+app.get("/dashboard", sessionsMiddleware, (c) =>
+  c.html(serveHTML("home.html")),
+);
+app.get("/dashboard/alerts", sessionsMiddleware, (c) =>
   c.html(serveHTML("alerts.html")),
 );
-app.get("/dashboard/settings", sessionMiddleware, (c) =>
+app.get("/dashboard/settings", sessionsMiddleware, (c) =>
   c.html(serveHTML("settings.html")),
 );
-app.get("/dashboard/admin", sessionMiddleware, (c) =>
+app.get("/dashboard/admin", sessionsMiddleware, (c) =>
   c.html(serveHTML("admin.html")),
 );
 
--- src/middlewares/sessionMiddleware.ts
+++ src/middlewares/sessionMiddleware.ts
@@ -1,7 +1,7 @@
 import { Context, Next } from "hono";
 import { verifyToken } from "../utils/token";
 
-export const sessionMiddleware = async (c: Context, next: Next) => {
+export const sessionsMiddleware = async (c: Context, next: Next) => {
   const session = c.get("session");
   const jwt = session.get("jwt");
 
