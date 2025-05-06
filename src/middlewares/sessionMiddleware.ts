821763678837e2bad63c7ba9be8aeb46a8a02b32
Kim <saludeskimdev@gmail.com>
2025-05-06 12:41:09 +0800

    Refactor session middleware; improve JWT handling and redirect logic

--- src/middlewares/sessionMiddleware.ts
+++ src/middlewares/sessionMiddleware.ts
@@ -2,16 +2,30 @@ import { Context, Next } from "hono";
 import { verifyToken } from "../utils/token";
 
 export const sessionsMiddleware = async (c: Context, next: Next) => {
+  if (c.req.path === "/") {
+    return await next();
+  }
+
   const session = c.get("session");
   const jwt = session.get("jwt");
+
+  if (!jwt) {
+    session.forget("id");
+    session.forget("jwt");
+    return c.redirect("/");
+  }
 
   try {
     const decoded = verifyToken(jwt);
     c.set("user", decoded);
     await next();
+
+    if (c.req.path === "/") {
+      c.redirect("/dashboard");
+    }
   } catch (err) {
     session.forget("id");
     session.forget("jwt");
-    return c.redirect("/", 302);
+    return c.redirect("/");
   }
 };
