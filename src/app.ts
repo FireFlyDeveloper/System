730aa070258b53b692ecf36101b6e627b148ab75
Kim <saludeskimdev@gmail.com>
2025-05-06 11:32:24 +0800

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { Hono } from 'hono'
+import { serveStatic } from "hono/bun";
+import { serveHTML } from './helpers/serverHTML';
+
+const app = new Hono()
+
+app.use("/static/*", serveStatic({ root: "./src/" }));
+
+app.get("/", (c) => c.html(serveHTML("login.html")));
+app.get("/dashboard", (c) => c.html(serveHTML("home.html")));
+app.get("/dashboard/alerts", (c) => c.html(serveHTML("alerts.html")));
+app.get("/dashboard/settings", (c) => c.html(serveHTML("settings.html")));
+app.get("/dashboard/admin", (c) => c.html(serveHTML("admin.html")));
+
+export default app
